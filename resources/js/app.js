7cf317808ae223caf2f1d2e1d469045b696e1ab5
Ezhaym N <ezhaym@gmail.com>
2024-10-14 12:30:33 -0600

    Working Vuetify

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,10 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import vuetify from './vuetify'
 import App from './App.vue'
 
 const app = createApp(App);
 
+app.use(createPinia())
+app.use(vuetify)
 app.mount('#app');
